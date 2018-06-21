346fe536e495dd6af52e51dc4fb0f8e82d27d9dc
Shoriful Siddiki <Heat@172-17-64-171.dynapool.nyu.edu>
2018-06-20 20:43:02 -0400

    adding phones to GitHub

---
+++ phones.py
@@ -0,0 +1,7 @@
+name = "Shoriful"
+phone = "588-7355"
+ac = '(917)'
+hi = 'Hi my name is {} and my area code is {}'.format(name,ac)
+full_number = 'My number is {}'.format(phone)
+print(hi)
+print(full_number)
